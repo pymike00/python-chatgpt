533b484090c55e1318e78553db4bd49f8c8aa13b
Michele Saba <32687496+pymike00@users.noreply.github.com>
2023-12-07 19:09:15 +0100

    UPDATE: openai v1 api integration

--- NEXUS/assistant.py
+++ NEXUS/assistant.py
@@ -1,15 +1,21 @@
 import json
-import openai
+from openai import OpenAI
 
-with open("secrets.json") as f:
-    secrets = json.load(f)
-    api_key = secrets["api_key"]
 
-openai.api_key = api_key
+try:
+    with open("secrets.json") as f:
+        secrets = json.load(f)
+        api_key = secrets["api_key"]
+    client = OpenAI(api_key=api_key)
+except FileNotFoundError:
+    print("The file 'secrets.json' was not found.")
+except KeyError:
+    print("The 'api_key' was not found in 'secrets.json'.")
 
-def get_response(messages:list):
-    response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
+
+def get_chat_response(messages: list[dict]):
+    response = client.chat.completions.create(
+        model = "gpt-4", # or "gpt-3.5-turbo"
         messages=messages,
         temperature = 1.0 # 0.0 - 2.0
     )
@@ -17,15 +23,19 @@ def get_response(messages:list):
 
 
 if __name__ == "__main__":
+    assistant_name = "Joi"
     messages = [
-        {"role": "system", "content": "Sei un assistente virtuale chiamata JOI e parli italiano."}
+        {
+            "role": "system", 
+            "content": f"Sei un assistente virtuale chiamata {assistant_name} e parli italiano."
+        }
     ]
     try:
         while True:
             user_input = input("\nTu: ")
             messages.append({"role": "user", "content": user_input})
-            new_message = get_response(messages=messages)
-            print(f"\nJOI: {new_message['content']}")
+            new_message = get_chat_response(messages=messages)
+            print(f"\n{assistant_name}: {new_message.content}")
             messages.append(new_message)
     except KeyboardInterrupt:
         print("A presto!")
